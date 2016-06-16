51d2c3c509db9f0d9ccfb893b0ec419a34e7936d
handioq <ter.alexanderandr@gmail.com>
2016-06-16 23:48:22 +0300

    Multiple Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 export class Hero {
   id: number;
@@ -22,21 +24,15 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template:`
   <h1>{{title}}</h1>
-  	<h2>My Heroes</h2>
-	<ul class="heroes">
+  <h2>My Heroes</h2>
+  <ul class="heroes">
     <li *ngFor="let hero of heroes"
-		[class.selected]="hero === selectedHero"(click)="onSelect(hero)">
-		<span class="badge">{{hero.id}}</span> {{hero.name}}
-		</li>
-    </ul>
-	<div *ngIf="selectedHero">
-	<h2>{{selectedHero.name}} details!</h2>
-	<div><label>id: </label>{{selectedHero.id}}</div>
-	<div>
-		<label>name: </label>
-		<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-	</div>
-	</div>
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
@@ -86,7 +82,8 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+  `],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
